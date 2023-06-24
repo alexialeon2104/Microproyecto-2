5e584fb8975c9fa9395fa86a75e4f99afa5c4abf
alexialeon2104 <a.leon2020@correo.unimet.edu.ve>
2023-06-24 04:36:53 -0400

    home casi listo

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,33 +2,36 @@ import React from "react";
 import styles from "./Card.module.css";
 import { Link } from "react-router-dom";
 import { MOVIE_DETAIL_URL } from "../../urls/urls";
+import {IMAGE_PATH} from "../../utils/api-themoviebd"
 
 
-
-function Card({  movie }) {
+function Card({ movie}) {
     return (
-      <div className={styles.container}>
-        <div>
-          <img
-            src={movie.image}
-            alt={movie.name}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.rightSideContainer}>
-          <div className={styles.infoContainer}>
-            <h2 className={styles.name}>
-              <Link
-                to={MOVIE_DETAIL_URL(movie.id)}
-                className={styles.link}
-              >
-                {movie.name}
-              </Link>
-            /</h2>
-            
-           
-            </div>
+     
+        <div className={styles.container}>
+          <div>
+            <img
+              src={IMAGE_PATH + movie.poster_path}
+              alt={movie.origina_title}
+              className={styles.image}
+            />
           </div>
+          <div className={styles.rightSideContainer}>
+            <div className={styles.infoContainer}>
+              <h2 className={styles.name}>
+                <Link
+                  to={MOVIE_DETAIL_URL(movie.id)}
+                  className={styles.link}
+                >
+                  {movie.title}
+                </Link>
+              </h2>
+              <h3 className={styles.subtitleInfo}>Idiomas: {movie.original_language}</h3>
+              <h3 className={styles.subtitleInfo}>Géneros: {movie.genre_ids}</h3>
+              
+              </div>
+            </div>
+         
           {/*<div className={styles.infoContainer}>
             <h3 className={styles.subtitle}>Last known location:</h3>
             <h3 className={styles.subtitleInfo}>
